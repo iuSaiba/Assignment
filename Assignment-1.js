811757290ab87716764d3ba0dc6ff55308f915c0
iuSaiba <intiseryusrasaiba@gmail.com>
2021-08-15 21:48:19 +0600

    Assignment

---
+++ Assignment-1.js
@@ -0,0 +1,25 @@
+// 1.
+function feetToInch(feet) {
+    var feet = feet / 12;
+    return feet;
+};
+console.log('feet To Inch');
+// 2.
+function centimeterToMeter (centimeter) {
+    var centimeter = centimeter / 3;
+    return centimeter;
+};
+console.log('centimeter To Meter');
+// 3.
+function paperRequirements(paperNumber1, paperNumber2, paperNumber3) {
+    let sum = (paperNumber1 + paperNumber2 + paperNumber3);
+    return sum;
+};
+console.log(paperRequirements(100 ,200 ,300));
+// 4.
+function bestFriend(_Fariends) {
+    let Fariends = ['Kaniz Fatema', 'Masfi', 'Zara', 'Hindo Farind'];
+    return 'Kaniz Fatema'
+}
+console.log('My Favorite Fariend Kaniz Fatema')
+bestFriend()
